84966bb3183bd50312a3d0a03b8411f75b09694a
Pereverzev Oleksandr <oleksandr.pereverzev@smartyads.com>
2022-11-26 19:09:40 +0200

    first change

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,13 @@ public class Main {
         System.out.println("Please, enter your name");
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
-
+        //comment only for github change
         System.out.printf("Your name is %s \n",name);
         Concret test1 = new Concret(10,15.2f);
         System.out.println(test1.getName());
         System.out.println(test1.getI());
         System.out.println(test1.getF());
+
     }
 
 
